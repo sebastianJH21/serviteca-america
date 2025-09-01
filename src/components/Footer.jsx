61c540366846d2d49e5a22be3825e2b3cca8527d
Sebastian Jimenez <sjimnezc@cesde.net>
2025-09-01 03:10:06 -0500

    Se crea boton de whatsapp y se agregar información del footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,16 @@
 import './Footer.css';
 
 function Footer() {
+  function currentYear() {
+    return new Date().getFullYear();
+  }
 
   return (
     <div className="copyright_section">
       <div className="location_main">
         <div className="location_text">
           <ul>
-            <li><a href="#"><i className="fa fa-phone" aria-hidden="true"></i><span className="padding_left_15">(+71)
-              8522369417</span></a></li>
+            <li><a href="#"><i className="fa fa-phone" aria-hidden="true"></i><span className="padding_left_15">(+57) 3147795448</span></a></li>
             <li><a href="#"><i className="fa fa-envelope" aria-hidden="true"></i><span
               className="padding_left_15">demo@gmail.com</span></a></li>
           </ul>
@@ -19,12 +21,15 @@ function Footer() {
           <li><a href="#"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
           <li><a href="#"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
           <li><a href="#"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
-          <li><a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a></li>
+          <li><a href="https://www.instagram.com/serviteca_america/"><i className="fa fa-instagram" aria-hidden="true"></i></a></li>
         </ul>
       </div>
       <div className="container">
-        <p className="copyright_text">2020 All Rights Reserved. Design by <a href="https://html.design">Free Html Templates</a>. DIstributed by <a href="https://themewagon.com" target="_blank">ThemeWagon</a></p>
+        <p className="copyright_text">{currentYear()} All Rights Reserved. Design by <a href="https://html.design">Sebastian C. Jimenez H</a>.</p>
       </div>
+      <a href="https://wa.link/m025qv" className="whatsapp-float" target="_blank" aria-label="Chat en WhatsApp">
+        <i className="fa fa-whatsapp" aria-hidden="true"></i>
+      </a>
     </div>
   );
 }
